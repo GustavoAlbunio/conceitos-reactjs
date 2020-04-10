da4d0fa082e7c632f402281271e3a62965629f1c
Gustavo Albunio <gustavo.albunio.grupoacert@gmail.com>
2020-04-10 18:39:02 -0300

    challenge completation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: "Conceitos de ReactJS",
+      url: "https://github.com/GustavoAlbunio/conceitos-reactjs",
+      techs: ["ReactJS", "Axios", "Jest"],
+    });
+
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
+  }
+
+  async function handleLikeRepository(id) {
+    const { data } = await api.post(`repositories/${id}/like`);
+
+    setRepositories(
+      repositories.map((repository) => {
+        if (repository.id === data.id) {
+          return { ...repository, likes: data.likes };
+        }
+        return repository;
+      })
+    );
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.length > 0 &&
+          repositories.map((repository) => (
+            <li key={repository.id}>
+              {repository.title}
+              <button
+                className="delete"
+                onClick={() => handleRemoveRepository(repository.id)}
+              >
+                Remover
+              </button>
+              <button
+                className="like"
+                onClick={() => handleLikeRepository(repository.id)}
+              >
+                <span>{repository.likes}</span>
+                Like
+              </button>
+            </li>
+          ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
